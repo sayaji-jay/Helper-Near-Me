9d2f6428563d092d44ee3dae92fcd0e499f2830f
jay19patel <justjayy19@gmail.com>
2025-11-17 15:47:08 +0530

    feat: Add Google Analytics integration to layout

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { APP_NAME } from "@/lib/constants";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,6 +27,25 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+      
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-M3C1VX9M59"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+        >
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-M3C1VX9M59');
+          `}
+        </Script>
+      
+      
       </head>
       <body className={`${inter.variable} font-sans antialiased min-h-screen`}>
         <Header />
